c904e2f8b543f3e57d084ba1bad1d3f41d057d02
anubhavigahoi <107360017+anubhavigahoi@users.noreply.github.com>
2023-03-27 20:09:54 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# coffee_main_website
-A website for purchasing coffee related items technologies used are : HTML , CSS , JavaScript , php , MySql
+# coffee_website
---
+++ src/Data.js
@@ -0,0 +1,38 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Black Coffee",
+    price: 15.99,
+    img: "images/menu-1.png",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Espresso",
+    price: 17.99,
+    img: "images/menu-2.png",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Americano",
+    price: 20.99,
+    img: "images/menu-3.png",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Cold Brew",
+    price: 18.99,
+    img: "images/menu-4.png",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Cappuccino",
+    price: 14.99,
+    img: "images/menu-5.png",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Cold Coffee",
+    price: 16.99,
+    img: "images/menu-6.png",
+  },
+];
